12bf0e742fbabd41d1ee9fc2469e9bebc931bb05
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-07-03 07:56:55 +0200

    dont cache redirects

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,19 @@ router.get('/',  function (req, res, next) {
     return userAgent.toLowerCase().indexOf("iphone") > -1;
   }
 
-  if (req.query.redirect) {
-  return next();
-  }
-
   var userAgent = req.headers['user-agent'];
 
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+
   if (isAndroid(userAgent)) {
-  return res.redirect("https://play.google.com/store/apps/details?id=im.status.ethereum");
+    return res.redirect("https://play.google.com/store/apps/details?id=im.status.ethereum");
   } else if (isIOS(userAgent)) {
-  return res.redirect("https://status.im/success")
+    return res.redirect("https://status.im/success")
   }
 
   return res.redirect("https://status.im")
-
 })
 
 router.get('/health', function(req, res) {
