4ac1049fa7633f21e2d0f93afa1ece44e8464ae3
Julien Eluard <julien.eluard@gmail.com>
2019-03-26 14:52:28 +0100

    Removed old morgan dependency

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
@@ -12,7 +11,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
